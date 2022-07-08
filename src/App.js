b20eafbc24fc8e9eecf81f374da4c13a0ab4b89d
delaneytiger21 <forsythedelaney@gmail.com>
2022-07-08 13:20:08 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <Weather />
-      <footer>
-        Built by Delaney Tiger, open-sourced on{" "}
-        <a
-          href="https://github.com/delaneytiger21/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://ubiquitous-kangaroo-0f21bf.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer className="mt-3">
+          Built by Delaney Tiger, open-sourced on{" "}
+          <a
+            href="https://github.com/delaneytiger21/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://ubiquitous-kangaroo-0f21bf.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
